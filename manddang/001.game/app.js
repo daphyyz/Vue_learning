7af9400b02777004ca8dd82927e64e009149449d
daphyyz <icecre4m@gmail.com>
2021-07-15 13:42:23 +0900

    scissor rock paper - watch

--- manddang/001.game/app.js
+++ manddang/001.game/app.js
@@ -2,7 +2,24 @@ new Vue({
     el: "#app",
     data: {
         myChoice: null,
+        comChoice: null,
         count: 3
+    },
+    watch: {
+        count: function (newVal) {
+            if(newVal ===0) {
+                //console.log('카운트가 0이 되었습니다.')
+                //Math.random() 0.33, 0.66, 그외 나머지
+                let number = Math.random()
+                if(number < 0.33) {
+                    this.comChoice = 'scissor'
+                } else if(number < 0.66) {
+                    this.comChoice = 'rock'
+                } else {
+                    this.comChoice = 'paper'
+                }
+            }
+        }
     },
     methods: {
         startGame: function() {
@@ -19,4 +36,4 @@ new Vue({
             }
         }
     }
-});
+})
